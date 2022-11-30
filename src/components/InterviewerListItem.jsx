febc23d9579975364afe9c278220f28592fc3836
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-30 16:38:56 -0500

    Add InterviewerList jxs, scss and stories. added all componenents for InterviewerList including styles, and storybook test file.

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -4,16 +4,25 @@ import "components/InterviewerListItem.scss";
 import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar } = props;
+
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const intereviewerClass = (selected) => {
+    return selected ? "interviewers__item--selected" : "interviewers__item";
+  };
+
+  const showInterviewerName = (selected) => {
+    return selected ? name : null;
+  };
 
   return (
-    <li className="interviewers__item">
+    <li className={intereviewerClass(selected)} onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {name}
+      {showInterviewerName(selected)}
     </li>
   );
 }
