8e0eb476159ba7a98951f0d18ff6fbce57119863
doublefriedtofu <hurdonghyuk@gmail.com>
2022-12-01 18:06:29 -0500

    Add Header, Show and Empty views with conditional rendering

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import './styles.scss'
+import './styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
-export default function Appointment () {
+
+export default function Appointment(props) {
+  const { id, time, interview } = props;
+
   return (
-    <article className="appointment"></article>
-  )
+    <article className="appointment">
+      <Header time={time}>
+      </Header>
+      {interview && <Show student={interview.student} interviewers={interview.interviewer} />}
+      {!interview && <Empty />}
+    </article>
+  );
 }
