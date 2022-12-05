a0226cb4fb4f64443b0d96ab5410bb9fead8903a
doublefriedtofu <hurdonghyuk@gmail.com>
2022-12-04 21:21:14 -0500

    Build userVisualMode hook complete useVisual mode passed the jest tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const [value, setValue] = useState(initial);
+  const transition = ((newVal, replace = false) => {
+    if (replace) {
+      history.pop()
+      setHistory([...history, newVal]);
+      setMode(newVal);  
+    } else {
+      setHistory([...history, newVal]);
+      setMode(newVal);
+    }
+  });
 
-  return {
-    value,
-    onChange: (event) => setValue(event.target.value)
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
   };
+
+  return { mode: mode, transition: transition, back: back };
 }
